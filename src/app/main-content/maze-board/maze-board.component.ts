7e6137d056c2998a6bb75b5af7322362b5076bc5
Canary <magnit2132@gmail.com>
2021-03-21 20:20:41 +0200

    dijkstra alg, terrible realization

--- src/app/main-content/maze-board/maze-board.component.ts
+++ src/app/main-content/maze-board/maze-board.component.ts
@@ -1,4 +1,14 @@
-import {AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
 import {fromEvent} from 'rxjs';
 import {MouseEventService} from './mouse-event.service';
 import {MazeSquareComponent} from './maze-square/maze-square.component';
@@ -13,23 +23,31 @@ export class MazeBoardComponent implements OnInit, AfterViewInit {
 
   @ViewChild('board', {static: true}) board: ElementRef;
   @ViewChildren('cmp') comp: QueryList<MazeSquareComponent>;
-  boardSquares: {animation: string}[][];
+  boardSquares: {
+    animation: string,
+    [key: string]: any}[][];
 
-  constructor(private mouseEvent: MouseEventService) { }
+  start;
+  finish;
+
+  private rowsAmount = 30;
+  private cellsAmount = 50;
+
+  constructor(private mouseEvent: MouseEventService, private cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    const rowsAmount = 30;
-    const cellsAmount = 50;
 
     // use loop to gain better performance than fill and map
-    this.boardSquares = new Array(rowsAmount);
-    for (let i = 0; i < rowsAmount; i++) {
+    this.boardSquares = new Array(this.rowsAmount);
+    for (let i = 0; i < this.rowsAmount; i++) {
       this.boardSquares[i] = [];
-      for (let j = 0; j < cellsAmount; j++) {
+      for (let j = 0; j < this.cellsAmount; j++) {
         this.boardSquares[i][j] = {animation: ''};
       }
     }
     this.addMouseEvents();
+
+    this.dijkstraAlgrithm();
   }
 
   ngAfterViewInit(): void {
@@ -46,9 +64,81 @@ export class MazeBoardComponent implements OnInit, AfterViewInit {
     });
   }
 
-  boardSquaresCounter(number: number): Array<any>{
-    return new Array(number);
+  dijkstraAlgrithm() {
+    let quary = [];
+    let distances = new Map();
+    let previous = new Map();
+
+    this.boardSquares.forEach((row, rowIndex) => {
+      row.forEach((el, i) => {
+        el.row = rowIndex;
+        el.col = i;
+      });
+    });
+
+    const graph = this.boardSquares.flat();
+    graph.forEach((el, i) => {
+      distances.set(el, null);
+      previous.set(el, null);
+      quary.push(el);
+
+      if (i === 51){
+        distances.set(el, 0);
+        el.start = true;
+      }
+    });
+
+    this.boardSquares[10][1].finish = true;
+
+    let i = 0;
+    while (quary.length) {
+      const cur = quary.sort((a, b) => {
+        if (distances.get(a) === null) return 1;
+        if (distances.get(b) === null) return -1;
+        return distances.get(a) - distances.get(b);
+      })[0];
+
+      setTimeout(() => {
+        cur.animation = 'passed';
+        this.cd.detectChanges();
+      }, i * 100)
+
+
+      quary = quary.filter(el => el !== cur);
+
+      if (cur.finish) {
+        let path = cur;
+        while (path) {
+          const a = path;
+          setTimeout(() => {
+            a.animation = 'wall';
+            this.cd.detectChanges();
+          }, i * 100)
+          path = previous.get(path);
+          i++;
+        }
+        return;
+      }
+
+      i++;
+
+      const neighbors = this.getVertexNeighbors(cur);
+
+      neighbors.forEach(el => {
+        if (!distances.get(el) && quary.find(exist => exist === el)) {
+          distances.set(el, 1);
+          previous.set(el, cur);
+        }
+      })
+    }
   }
 
+  private getVertexNeighbors(vertex) {
+    let nextRow = vertex.row < this.rowsAmount - 1 ? [this.boardSquares[vertex.row + 1][vertex.col]] : [];
+    let prevRow = vertex.row > 0 ? [this.boardSquares[vertex.row - 1][vertex.col]] : [];
+    let nextCol = vertex.col < this.cellsAmount - 1 ? [this.boardSquares[vertex.row][vertex.col + 1]] : [];
+    let prevCol = vertex.col > 0 ? [this.boardSquares[vertex.row][vertex.col - 1]] : [];
+    return nextRow.concat(...nextCol, ...prevCol, ...prevRow);
+  }
 
 }
